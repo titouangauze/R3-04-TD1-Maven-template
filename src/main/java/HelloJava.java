24e6cf8c9ddeda82498dddb67911032439156f32
[titouan] <[titouannicolasgauze@gmail.com]>
2024-09-10 10:16:36 +0200

    test

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -1,6 +1,7 @@
 
 /**
  * Ceci est une classe de test qui ne fait pas grand chose
+ * avec une modification comme test
  *
  * @author JMB
  * @author LC
@@ -9,32 +10,31 @@ class HelloJava {
 
     private String ideForJava = "";
     private String comment = "";
+
     public void afficherCancan() {
         System.out.println("test");
 
     }
 
-    HelloJava(String ide , String comment){
+    HelloJava(String ide, String comment) {
         this.ideForJava = ide;
         this.comment = comment;
     }
 
-    public String getIde(){
+    public String getIde() {
         return this.ideForJava;
     }
 
-    public String setIde(String ide){
+    public String setIde(String ide) {
         return this.ideForJava = ide;
     }
 
-    public String getComment(){
+    public String getComment() {
         return this.comment;
     }
 
-    public String setComment(String comment){
+    public String setComment(String comment) {
         return this.comment = comment;
     }
 
-   
 }
-
